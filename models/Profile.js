a3ba2ce2f10f619cd848d0231444ef3453ff1aab
“Hash” <waghharshal405@gmail.com>
2019-12-05 15:54:07 +0530

    Atom editor commit

--- models/Profile.js
+++ models/Profile.js
@@ -1,90 +1,86 @@
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose' );
 
-const ProfileSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+const ProfileSchema = new mongoose.Schema( {
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [ String ],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [ {
+    title: {
+      type: String,
+      required: true
     },
     company: {
-        type: String
-    },
-    website: {
-        type: String
+      type: String,
+      required: true
     },
     location: {
-        type: String
+      type: String
     },
-    status: {
-        type: String,
-        required: true
+    from: {
+      type: Date,
+      required: true
     },
-    skills: {
-        type: [String],
-        required: true
+    to: {
+      type: Date,
     },
-    bio: {
-        type: String
+    current: {
+      type: Boolean,
+      default: false
     },
-    githubusername: {
-        type: String
+    description: {
+      type: String
+    }
+  } ],
+  education: [ {
+    school: {
+      type: String,
+      required: true
     },
-    experience: [
-        {
-            title: {
-                type: String,
-                required: true
-            },
-            company: {
-                type: String,
-                required: true
-            },
-            location: {
-                type: String
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date,
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String
-            }
-        }
-    ],
-    education: [
-        {
-            school: {
-                type: String,
-                required: true
-            },
-            degree: {
-                type: String,
-                required: true
-            },
-            fieldofstudy: {
-                type: String,
-                required: true
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date,
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String
-            }
-        }
-    ]
-});
+    degree: {
+      type: String,
+      required: true
+    },
+    fieldofstudy: {
+      type: String,
+      required: true
+    },
+    from: {
+      type: Date,
+      required: true
+    },
+    to: {
+      type: Date,
+    },
+    current: {
+      type: Boolean,
+      default: false
+    },
+    description: {
+      type: String
+    }
+  } ]
+} );
